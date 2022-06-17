e65a44ce80678ef4c6046fe8cd9e549d5f65600f
Anastasiia <nastiababych13@gmail.com>
2022-06-17 14:51:27 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,92 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../bubblesort/bubblesort.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(InputTestMethod1)
+		{
+			BubbleSort B;
+			int i;
+			int Array[] = { 1, 5, 8, 7, 7 };
+			int size = sizeof(Array);
+
+			B.Input(Array, 5);
+			Assert::IsTrue(5 == size);
+		}
+	};
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(InputTestMethod2)
+		{
+			BubbleSort B;
+			int Array[] = { 1,5,8,7,7 };
+			B.Input(Array, 5);
+			int x = Array[0];
+			Assert::IsTrue(1 == x);
+		}
+	};
+
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+
+		TEST_METHOD(SortTestMethod3)
+		{
+			BubbleSort B;
+			int i;
+			int Array[] = { 1,5,8,7,7 };
+			int SortArray[] = { 1,5,7,7,8 };
+			B.Input(Array, 5);
+			Assert::IsTrue(SortArray[i] <= SortArray[i + 1]);
+		}
+	};
+
+
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+
+		TEST_METHOD(SortTestMethod4)
+		{
+			BubbleSort B;
+			int z;
+			const unsigned int count = 5;
+			int Array[count] = { 5, 3, 10, 2, 7 };
+			int SortArray[count] = { 2, 3, 5, 7, 10 };
+
+			B.bubble_sort(Array, count);
+			for (int i = 0; i < count; i++)
+			{
+				Assert::AreEqual(SortArray[i], Array[i]);
+			}
+
+		};
+	};
+	TEST_CLASS(UnitTest5)
+	{
+	public:
+
+		TEST_METHOD(SortTestMethod5)
+		{
+			BubbleSort B;
+			const unsigned int count = 5;
+			int Array[count] = { 3, 3, 3, 3, 3 };
+			int SortedArray[count] = { 3, 3, 5, 3, 3 };
+
+			B.bubble_sort(Array, count);
+			for (int i = 0; i < count; i++)
+			{
+				Assert::AreEqual(SortedArray[i], Array[i]);
+			}
+		};
+	};
+}
---
+++ bubblesort/bubblesort.cpp
@@ -0,0 +1,69 @@
+﻿// bubblesort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class BubbleSort {
+public:
+    BubbleSort() {
+
+    }
+    void bubble_sort(int Array[], int size);
+    void Input(int Array[], int n);
+    void Print(int Array[], int n);
+    int Array[100];
+    int n;
+    int temp;
+    ~BubbleSort(){}
+};
+
+void BubbleSort:: Input(int Array[], int n) {
+    cout << "Enter values: "<<endl;
+    for (int i = 0; i <= n - 1; i++) {
+        cin >> Array[i];
+    }
+    cout << "Elements of your unsorted list:  ";
+    for (int i = 0; i <= n - 1; i++) {
+        cout << Array[i] << " ";
+    }
+    cout << endl;
+   
+}
+
+
+void BubbleSort::bubble_sort(int Array[], int n) {
+    for (int m = 0; m < n; m++)
+        for (int i = 0; i < n - m - 1; i++)
+            if (Array[i] > Array[i + 1]) {
+                temp = Array[i];
+                Array[i] = Array[i + 1];
+                Array[i + 1] = temp;
+            }
+   
+}
+
+
+void BubbleSort::Print(int Array[], int n) {
+
+   
+    cout << "Elements of your sorted list:  ";
+    for (int i = 0; i < n; i++) {
+        cout << Array[i] << " ";
+    }
+
+}
+
+int main() {
+    int n;
+    cout << "Enter the count of element: " ;
+    cin >> n;
+    int* Arr = new int[n];
+
+    BubbleSort B;
+    B.Input(Arr, n);
+    B.bubble_sort(Arr, n);
+    B.Print(Arr, n);
+    return 0;
+}
+
